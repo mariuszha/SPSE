9f74418d2b8f11b740fa802f421de23c18249777
Mariusz Hanuszczak <mariusz.hanuszczak@gmail.com>
2017-04-20 10:31:39 +0200

    Create ex.stat.file.py

---
+++ Module_2/Lesson_2/ex.stat.file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2.7
+
+# 
+# ex.stat.file.py v1.0 by mariuszha
+#
+
+import os
+import sys
+from stat import *
+
+top = os.getcwd()
+
+for f in os.listdir(top):
+	pathname = os.path.join(top, f)
+	mode_uid = os.stat(pathname).st_uid
+	mode_gid = os.stat(pathname).st_gid
+	mode_size = os.stat(pathname).st_size
+	mode_atime = os.stat(pathname).st_atime
+	mode_mtime = os.stat(pathname).st_mtime
+	print 'Path: %s' % pathname
+	print 'UID: %s' %  mode_uid
+	print 'GID: %s' % mode_gid
+	print 'SIZE: %s' % mode_size
+	print 'Access time: %s' % mode_atime
+	print 'Modification time: %s' % mode_mtime 
