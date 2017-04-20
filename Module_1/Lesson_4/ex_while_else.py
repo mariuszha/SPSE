554c0db2d953e0b033fb5dcb58815d4f3d78b44e
Mariusz Hanuszczak <mariusz.hanuszczak@gmail.com>
2017-04-20 10:06:05 +0200

    Create ex_while_else.py

---
+++ Module_1/Lesson_4/ex_while_else.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+
+# ex_while_else.py by mariuszha
+#
+# Excercise:
+# While loop with else 
+#
+
+i = 0
+
+while i < 5:
+	print i
+	print 'i < 5'
+	i += 1
+else:
+	print i
+	print 'i => 5'
+  
+print 'This is the last statement'
