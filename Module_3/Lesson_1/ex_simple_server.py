3c81a9856e98f4c29b7659691452bcb843dd088f
Mariusz Hanuszczak <mariusz.hanuszczak@gmail.com>
2017-08-23 21:32:05 +0200

    Create ex_simple_server.py

---
+++ Module_3/Lesson_1/ex_simple_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# ex_simple_server.py by mariuszha
+#
+# Purpose:
+# Create a simple Echo Server to handle 1 client 
+#
+
+import socket
+
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+bind_IP = '0.0.0.0'
+bind_port = 4444
+
+#tcpSocket.listen(1)
+
+
+tcpSocket.bind((bind_IP, bind_port))
+
+tcpSocket.listen(1)
+
+
+print 'Waiting for a client ...'
+
+(client, (ip, sock)) = tcpSocket.accept()
+
+print 'Receiving connection from %s' % ip
+
+data = client.recv(2048)
+
+client.send('Echo ' + data)
+
+print 'Receiving from the client: %s' % data
+
+client.close()
